fd034a976644143f0ad3015e5d436458a4443167
camilamzambrano-oss <camila.mzambrano@gmail.com>
2025-10-08 08:13:30 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $servidor = "sql306.infinityfree.com";
 $usuario = "if0_40113229"; 
 $clave = "bucaramangaSAN1";       
@@ -12,8 +9,5 @@ $conexion = new mysqli($servidor, $usuario, $clave, $bd);
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
-}
-     else {
-    echo "✅ Conexión exitosa a la base de datos";
 }
 ?>
